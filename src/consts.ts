3db347cf9f741975af72dfa80f9549389b339006
David Delord <david.delord@m2p-coaching.fr>
2026-06-01 16:07:14 +0200

    seo: title optimisé mots-clés

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'M²P Coaching & Développement';
+export const SITE_TITLE = 'Coach Professionnel Certifié RNCP 6 | Cadres & Dirigeants | Occitanie';
 export const SITE_DESCRIPTION = 'Coach professionnel certifié RNCP 6 - Un espace pour décider avec clarté '
