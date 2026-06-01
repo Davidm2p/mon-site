8e31cf3404940a698f14a81c13e5a80f9f9d2bb6
David Delord <david.delord@m2p-coaching.fr>
2026-06-01 11:29:49 +0200

    Site M2P complet

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'M²P Coaching & Développement';
+export const SITE_DESCRIPTION = 'Coach professionnel certifié RNCP 6 - Un espace pour décider avec clarté ';
