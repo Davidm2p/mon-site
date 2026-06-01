0078b1949af9b77822886bf8c4a2e66e0e1b9e20
David Delord <david.delord@m2p-coaching.fr>
2026-06-01 15:34:49 +0200

    fix: footer email lien corrigé

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'M²P Coaching & Développement';
-export const SITE_DESCRIPTION = 'Coach professionnel certifié RNCP 6 - Un espace pour décider avec clarté ';
+export const SITE_DESCRIPTION = 'Coach professionnel certifié RNCP 6 - Un espace pour décider avec clarté '
